0ae0edb51a9556f406f0a321c473f9c4b0ebfb06
Shawn <mathus13@gmail.com>
2015-08-14 01:36:28 +0000

    removing db settings

--- fuel/app/config/db.php
+++ fuel/app/config/db.php
@@ -6,5 +6,11 @@
  */
 
 return array(
-
+	'default' => array(
+		'connection'  => array(
+			'dsn'        => 'mysql:host=??;dbname=??',
+			'username'   => '',
+			'password'   => '',
+		),
+	),
 );
